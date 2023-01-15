ff365386c2be50ffbab7a7ffe7f11a98ade68f69
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2023-01-14 21:27:13 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 
 class Search extends React.Component {
+  state = {
+    banda: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { banda } = this.state;
+    const minInput = 2;
     return (
       <div data-testid="page-search">
-        Search
+        <form>
+          <label htmlFor="banda">
+            Banda ou artista:
+            <input
+              type="text"
+              id="banda"
+              name="banda"
+              data-testid="search-artist-input"
+              onChange={ this.handleChange }
+              value={ banda }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ banda.length >= minInput ? '' : 'disabled' }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }

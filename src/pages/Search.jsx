26dfecb1e662e3d9db62fcb7708dc51e6b780685
Sthéfani <sthefani_martinelli@hotmail.com>
2025-03-12 21:45:21 -0300

    Refatora componente Search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,87 +1,54 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import searchAlbumsAPI from "../services/searchAlbumsAPI";
-import LoadingMsg from "../components/Loading";
-import FormSearch from "../components/FormSearch";
-import AlbumCard from "../components/AlbumCard";
+import React, { useState } from 'react';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
+import Header from '../components/Header';
+import AlbumCard from '../components/AlbumCard';
 
-class Search extends React.Component {
-  state = {
-    banda: "",
-    onRequest: false,
-    resultadoRequest: [],
-    respostaPesquisa: false,
-    msgPagina: "",
+function Search() {
+  const [search, setSearch] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [albuns, setAlbuns] = useState(undefined);
+
+  const handleChangeInput = ({ target: { value } }) => {
+    setSearch(value);
   };
 
-  searchArtistOrAlbum = async () => {
-    const { banda } = this.state;
-    this.setState({
-      onRequest: true,
-    });
-    const searchAlbum = await searchAlbumsAPI(banda);
-    if (searchAlbum.length === 0) {
-      this.setState({
-        onRequest: false,
-        msgPagina: "Nenhum álbum foi encontrado",
-      });
-    } else {
-      this.setState({
-        onRequest: false,
-        msgPagina: `Resultado de álbuns de: ${banda}`,
-      });
-    }
-    // console.log(searchAlbum);
-    this.setState({
-      banda: "",
-      resultadoRequest: searchAlbum,
-      respostaPesquisa: true,
-    });
+  const handleButtonClick = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const result = await searchAlbumsAPI(search);
+    setAlbuns(result.length > 0 ? result : undefined);
+    setSearch('');
+    setIsLoading(false);
   };
 
-  handleChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  render() {
-    const { banda, onRequest, resultadoRequest, respostaPesquisa, msgPagina } =
-      this.state;
-    const minInput = 2;
-    return (
-      <div data-testid="page-search">
-        {onRequest && <LoadingMsg />}
-        <FormSearch
-          banda={banda}
-          minInput={minInput}
-          handleChange={this.handleChange}
-          searchArtistOrAlbum={this.searchArtistOrAlbum}
+  return (
+    <div data-testid="page-search">
+      <Header />
+      <form>
+        <input
+          type="text"
+          value={ search }
+          data-testid="search-artist-input"
+          onChange={ handleChangeInput }
         />
-        {respostaPesquisa && (
-          <div>
-            <p>{msgPagina}</p>
-            {resultadoRequest.map(
-              ({ collectionId, artworkUrl100, collectionName, artistName }) => (
-                <Link
-                  key={collectionId}
-                  to={`/album/${collectionId}`}
-                  data-testid={`link-to-album-${collectionId}`}
-                >
-                  <AlbumCard
-                    url={artworkUrl100}
-                    banda={artistName}
-                    album={collectionName}
-                  />
-                </Link>
-              )
-            )}
-          </div>
-        )}
-      </div>
-    );
-  }
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ search.length < 2 }
+          onClick={ handleButtonClick }
+        >
+          Pesquisar
+        </button>
+      </form>
+      {isLoading && <Loading />}
+      {albuns === undefined && <p>Nenhum álbum foi encontrado</p>}
+      {albuns?.length > 0 && <p>{`Resultado de álbuns de: ${albuns[0].artistName}`}</p>}
+      {albuns && albuns.map((album) => (
+        <AlbumCard key={ album.collectionId } album={ album } />
+      ))}
+    </div>
+  );
 }
 
 export default Search;
